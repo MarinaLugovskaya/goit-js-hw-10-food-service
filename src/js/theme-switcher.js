8ed32a512fb4a82932549b9b4395411c29d9442b
Marina Lugovskaya <anomaliya1993@gmail.com>
2021-06-27 14:49:34 +0300

    bagfix

--- src/js/theme-switcher.js
+++ src/js/theme-switcher.js
@@ -25,26 +25,3 @@ if (localStorage.getItem('theme') === Theme.DARK) {
   document.body.classList.add(Theme.DARK);
   themeSwitch.checked = true;
 }
-
-
-
-// function changeThemeDarkOnClick(event){
-// if(localStorage.getItem('theme') === Theme.LIGHT){
-//   document.body.classList.add('Theme.DARK');
-//   document.body.classList.remove('Theme.LIGHT');
-//   localStorage.setItem('theme', 'dark');
-// }
-// }
-
-// function changeThemeLightOnClick(event){
-//   if(localStorage.theme === Theme.DARK){
-//     document.body.classList.add('Theme.LIGHT');
-//     document.body.classList.remove('Theme.DARK');
-//     localStorage.setItem('theme', 'light');
-//   }
-//   }
-
-// if (localStorage.getItem('theme') === Theme.LIGHT) {
-//   document.body.classList.add(Theme.LIGHT);
-//   themeSwitch.checked = true;
-// }
